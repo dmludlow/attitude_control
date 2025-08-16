bd5c4fe08b32b0203671a98690407a5935737e21
Daniel Ludlow <danielludlow@Mac-15094.lan>
2025-08-15 21:11:28 -0700

    Initial commit

---
+++ state.py
@@ -0,0 +1,7 @@
+class State:
+    def __init__(self,q,w):
+        self.q = q
+        self.w = w
+
+
+# This is where I put state and related methods for a spacecraft class or something to use in conjucntion with its own geometry
