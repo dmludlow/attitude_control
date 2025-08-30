75cefc0ebca8175aa2b6d40ea63c75909dfef7ee
Daniel Ludlow <dmludlow@ucla.edu>
2025-08-29 17:51:33 -0700

    Updates to controller structure and organization. Attempting to resolve package import issues.

--- controllers/PD_control.py
+++ controllers/PD_control.py
@@ -3,11 +3,8 @@ This file contains implimentation of a PD controller for spacecraft attitude con
 """
 
 import numpy as np
-import attitude_control.spacecraft as sc
-import attitude_control.quaternion as qm
-import attitude_control.dynamics as dy
-import attitude_control.state as st
 import attitude_control.controllers as ctrl
+import attitude_control.spacecraft as sc
 
 class PD_control(ctrl.controller):
 
@@ -16,34 +13,37 @@ class PD_control(ctrl.controller):
     Kd: float
 
     def __init__(self, Kp: float, Kd: float):
-        '''
+        """
         Initializes the PD controller with specified gains.
 
         Args:
             Kp (float): Proportional gain.
             Kd (float): Derivative gain.
             craft (sc.Spacecraft): The spacecraft to be controlled.
-        '''
+        """
         self.Kp = Kp
         self.Kd = Kd
-        # Initialize control torque to zero
 
     # Very incomplete, just a starting point to make sure controller can act on the spacecraft
-    def torque(self, q_d: np.ndarray, w_d: np.ndarray) -> np.ndarray:
-        '''
-        Computes the control torque based on desired quaternion and angular velocity.
-        '''
-        # Current state
-        q = self.craft.state.q
-        w = self.craft.state.w
+    def torque(self, state_in: sc.State, state_desired: sc.State, I: np.ndarray) -> np.ndarray:
+        """
+        Computes the control torque.
 
+        Args:
+            state_in (sc.State): Current state of the spacecraft.
+            state_desired (sc.State): Desired state of the spacecraft.
+            I (np.ndarray): Inertia tensor of the spacecraft.
+
+        Returns:
+            np.ndarray: Control torque vector.
+        """
         # Quaternion error (((((not done yet)))))
-        q_e = 1
+        q_e = state_desired.q - state_in.q  # Placeholder for quaternion error calculation
 
         # Angular velocity error
-        w_e = w_d - w
+        w_e = state_desired.w - state_in.w
 
         # Controller torque calculation
-        self.T_c = self.Kp * q_e - self.Kd * w_e
-        return self.T_c
+        T_c = self.Kp * q_e - self.Kd * w_e
+        return T_c
     
