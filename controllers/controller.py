c7722da34de62c21b945cfd343274be5904e908f
Daniel Ludlow <dmludlow@ucla.edu>
2025-08-29 21:43:06 -0700

    Added PID controller (untested implimentation).

--- controllers/controller.py
+++ controllers/controller.py
@@ -12,8 +12,23 @@ class controller(ABC):
     def __init__(self, max_torque: float):
         """
         Initializes the controller.
-        
+
         Args:
             max_torque (float): Maximum allowable torque for the controller.
         """
         self.max_torque = max_torque
+
+    @abstractmethod
+    def torque(self, state_in, state_desired, I: np.ndarray) -> np.ndarray:
+        """
+        Abstract method to compute the control torque.
+
+        Args:
+            state_in: Current state of the spacecraft.
+            state_desired: Desired state of the spacecraft.
+            I (np.ndarray): Inertia tensor of the spacecraft.
+
+        Returns:
+            np.ndarray: Control torque vector.
+        """
+        pass
